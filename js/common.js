7c6d1bb4413dda2a14a9144a83426e3e458a8242
Timosh Yuriy <sfpwolf@mail.ru>
2017-05-14 00:28:03 +0300

    Pre-last commit

--- js/common.js
+++ js/common.js
@@ -40,4 +40,31 @@ $(document).ready(function() {
       itemsTablet: false,
       itemsMobile : false
      }); 
+   $("#owl-demo_4").owlCarousel({
+    navigation : false, // показывать кнопки next и prev 
+ 
+      slideSpeed : 300,
+      paginationSpeed : 400,
+ 
+      items : 1, 
+      itemsDesktop : false,
+      itemsDesktopSmall : false,
+      itemsTablet: false,
+      itemsMobile : false
+     }); 
+   var pop = document.getElementById("pop");
+   var pop2 = document.getElementById("pop2");
+   var popup = document.getElementById("popup");
+  var popup_2 = document.getElementById("popup_2");
+   pop.onclick = function() {
+   	 pop.style.className = "b-popup";
+   	 popup.style.display = "block";
+   	  
+
+   	};
+
+   	popup.onclick = function() {
+   		popup.style.display = "none";
+   	};
+  
 });
